cfafa5dad27bbb671c7b1ef63044c483448fe31c
mekdooms <mehdielkodssi@hotmail.fr>
2016-07-30 21:18:19 +0200

    cb almost ready

--- kitchen-sink-ios/js/kitchen-sink.js
+++ kitchen-sink-ios/js/kitchen-sink.js
@@ -206,72 +206,21 @@ myApp.onPageInit('credit-card', function (page) {
         ]
     });
 
-    var today = new Date();
-    
-
-    // Inline date-time
-    var pickerInline = myApp.picker({
-        input: '#ks-picker-date',
-        container: '#ks-picker-date-container',
-        toolbar: false,
+    // Describe yourself picker
+    var pickerExpire = myApp.picker({
+        input: '#ks-picker-expire',
         rotateEffect: true,
-        value: [today.getMonth(), today.getDate(), today.getFullYear(), today.getHours(), (today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes())],
-        onChange: function (picker, values, displayValues) {
-            var daysInMonth = new Date(picker.value[2], picker.value[0]*1 + 1, 0).getDate();
-            if (values[1] > daysInMonth) {
-                picker.cols[1].setValue(daysInMonth);
-            }
-        },
-        formatValue: function (p, values, displayValues) {
-            return displayValues[0] + ' ' + values[1] + ', ' + values[2] + ' ' + values[3] + ':' + values[4];
-        },
         cols: [
-            // Months
             {
-                values: ('0 1 2 3 4 5 6 7 8 9 10 11').split(' '),
-                displayValues: ('January February March April May June July August September October November December').split(' '),
-                textAlign: 'left'
+                textAlign: 'center',
+                values: ['01', '02', '03', '04']
             },
-            // Days
             {
-                values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+                textAlign: 'center',
+                values: ['2016', '2017', '2018', '2019']
             },
-            // Years
-            {
-                values: (function () {
-                    var arr = [];
-                    for (var i = 1950; i <= 2030; i++) { arr.push(i); }
-                    return arr;
-                })(),
-            },
-            // Space divider
-            {
-                divider: true,
-                content: '&nbsp;&nbsp;'
-            },
-            // Hours
-            {
-                values: (function () {
-                    var arr = [];
-                    for (var i = 0; i <= 23; i++) { arr.push(i); }
-                    return arr;
-                })(),
-            },
-            // Divider
-            {
-                divider: true,
-                content: ':'
-            },
-            // Minutes
-            {
-                values: (function () {
-                    var arr = [];
-                    for (var i = 0; i <= 59; i++) { arr.push(i < 10 ? '0' + i : i); }
-                    return arr;
-                })(),
-            }
         ]
-    });
+    });    
 
 });
 
