a49ccb1529ae343f970d7a2d97e2daa3536639e6
mekdooms <mehdielkodssi@hotmail.fr>
2016-07-30 21:07:47 +0200

    page signin click confirm

--- kitchen-sink-ios/js/kitchen-sink.js
+++ kitchen-sink-ios/js/kitchen-sink.js
@@ -161,6 +161,123 @@ myApp.onPageInit('swipe-delete modals media-lists', function (page) {
 
 });
 
+
+
+
+/* ===== Credit Card ===== */
+myApp.onPageInit('credit-card', function (page) {
+    var pickerCard = myApp.picker({
+        input: '#ks-picker-card',
+        cols: [
+            {
+                textAlign: 'center',
+                values: ['American Express', 'Master Card', 'VISA']
+            }
+        ]
+    });
+
+    var pickerCode = myApp.picker({
+        input: '#ks-picker-code',
+        cols: [
+            {
+                textAlign: 'center',
+                values: ['000', '111', '222', '333', '444']
+            }
+        ]
+    });
+
+    // Describe yourself picker
+    var pickerNumbers = myApp.picker({
+        input: '#ks-picker-numbers',
+        rotateEffect: true,
+        cols: [
+            {
+                textAlign: 'center',
+                values: ['00', '01', '10', '11']
+            },
+            {
+                textAlign: 'center',
+                values: ['00', '01', '10', '11']
+            },
+            {
+                textAlign: 'center',
+                values: ['00', '01', '10', '11']
+            },
+        ]
+    });
+
+    var today = new Date();
+    
+
+    // Inline date-time
+    var pickerInline = myApp.picker({
+        input: '#ks-picker-date',
+        container: '#ks-picker-date-container',
+        toolbar: false,
+        rotateEffect: true,
+        value: [today.getMonth(), today.getDate(), today.getFullYear(), today.getHours(), (today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes())],
+        onChange: function (picker, values, displayValues) {
+            var daysInMonth = new Date(picker.value[2], picker.value[0]*1 + 1, 0).getDate();
+            if (values[1] > daysInMonth) {
+                picker.cols[1].setValue(daysInMonth);
+            }
+        },
+        formatValue: function (p, values, displayValues) {
+            return displayValues[0] + ' ' + values[1] + ', ' + values[2] + ' ' + values[3] + ':' + values[4];
+        },
+        cols: [
+            // Months
+            {
+                values: ('0 1 2 3 4 5 6 7 8 9 10 11').split(' '),
+                displayValues: ('January February March April May June July August September October November December').split(' '),
+                textAlign: 'left'
+            },
+            // Days
+            {
+                values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+            },
+            // Years
+            {
+                values: (function () {
+                    var arr = [];
+                    for (var i = 1950; i <= 2030; i++) { arr.push(i); }
+                    return arr;
+                })(),
+            },
+            // Space divider
+            {
+                divider: true,
+                content: '&nbsp;&nbsp;'
+            },
+            // Hours
+            {
+                values: (function () {
+                    var arr = [];
+                    for (var i = 0; i <= 23; i++) { arr.push(i); }
+                    return arr;
+                })(),
+            },
+            // Divider
+            {
+                divider: true,
+                content: ':'
+            },
+            // Minutes
+            {
+                values: (function () {
+                    var arr = [];
+                    for (var i = 0; i <= 59; i++) { arr.push(i < 10 ? '0' + i : i); }
+                    return arr;
+                })(),
+            }
+        ]
+    });
+
+});
+
+
+
+
 /* ===== Messages Page ===== */
 myApp.onPageInit('messages', function (page) {
     var conversationStarted = false;
